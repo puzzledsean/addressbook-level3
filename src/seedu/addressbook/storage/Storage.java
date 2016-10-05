503c8229dff1ddf2bb8bce0b25b6aa3f2049306b
puzzledsean <puzzledsean@gmail.com>
2016-10-05 15:51:13 +0800

    added dependency injection principle

---
+++ src/seedu/addressbook/storage/Storage.java
@@ -0,0 +1,42 @@
+package seedu.addressbook.storage;
+
+import seedu.addressbook.data.AddressBook;
+import seedu.addressbook.data.exception.IllegalValueException;
+
+public abstract class Storage {
+
+    /**
+     * Shows that the given file path conflicts with filepath constraints.
+     */
+    public static class InvalidStorageFilePathException extends IllegalValueException {
+        public InvalidStorageFilePathException(String message) {
+            super(message);
+        }
+    }
+
+    /**
+     * Shows that an error occurred while trying to convert and read/write data between the application
+     * and the storage file.
+     */
+    public static class StorageOperationException extends Exception {
+        public StorageOperationException(String message) {
+            super(message);
+        }
+    }
+    
+    /**
+     * Saves all data to this storage location.
+     *
+     * @throws StorageOperationException if there were errors converting and/or storing data to file.
+     */
+    public abstract void save(AddressBook addressBook) throws StorageOperationException;
+    
+    /**
+     * Loads data from this storage file.
+     *
+     * @throws StorageOperationException if there were errors reading and/or converting data from file.
+     */
+    public abstract AddressBook load() throws StorageOperationException;
+    
+    public abstract String getPath();
+}
